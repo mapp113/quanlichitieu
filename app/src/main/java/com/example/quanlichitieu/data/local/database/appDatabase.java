6305a67466e5649533c685102e5081faf39dc4b3
thag89 <146001572+thag89@users.noreply.github.com>
2025-07-22 21:54:41 +0700

    Revert "Feat: CRUD Transaction"

--- app/src/main/java/com/example/quanlichitieu/data/local/database/appDatabase.java
+++ app/src/main/java/com/example/quanlichitieu/data/local/database/appDatabase.java
@@ -1,28 +1,4 @@
 package com.example.quanlichitieu.data.local.database;
 
-import android.content.Context;
-
-import androidx.room.Database;
-import androidx.room.Room;
-import androidx.room.RoomDatabase;
-
-import com.example.quanlichitieu.data.local.dao.TransactionDao;
-import com.example.quanlichitieu.data.local.entity.Transaction;
-
-@Database(entities = {Transaction.class}, version = 1, exportSchema = false)
-public abstract class appDatabase extends RoomDatabase{
-    private static appDatabase instance;
-
-    public abstract TransactionDao transactionDao();
-
-    public static synchronized appDatabase getDatabase(Context context) {
-        if (instance == null) {
-            instance = Room.databaseBuilder(context.getApplicationContext(),
-                            appDatabase.class, "transaction_db")
-                    .fallbackToDestructiveMigration()
-                    .build();
-        }
-        return instance;
-    }
-
+public class appDatabase {
 }
--- app/src/main/java/com/example/quanlichitieu/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/quanlichitieu/ui/activity/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.quanlichitieu.ui.activity;
 
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,15 +21,6 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button btnOpenTransaction = findViewById(R.id.btnOpenTransaction);
-        btnOpenTransaction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, TransactionActivity.class);
-                startActivity(intent);
-            }
         });
     }
 }
