edf910686f31eaca3f30b2d8a421da32ad4df79e
SonCode123 <thaison.kashima@gmail.com>
2025-07-24 02:15:09 +0700

    add menu bar

--- app/src/main/java/com/example/quanlichitieu/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/quanlichitieu/ui/activity/LoginActivity.java
@@ -20,6 +20,7 @@ import com.example.quanlichitieu.data.SessionManager;
 import com.example.quanlichitieu.data.local.database.appDatabase;
 import com.example.quanlichitieu.data.local.entity.User;
 import com.example.quanlichitieu.data.local.utils.PasswordUtils;
+import com.example.quanlichitieu.ui.activity.goal.GoalActivity;
 
 public class LoginActivity extends AppCompatActivity {
     EditText usernameEditText, passwordEditText;
